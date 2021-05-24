f992fd47aeeb66f972f1449071066a6bc4334045
gidwikintan <gidwikintan@gmail.com>
2021-05-24 11:31:16 +0700

    react components part 2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import {Navbar, NavbarBrand} from 'reactstrap';
+import Menu from './components/MenuComponent';
 import './App.css';
+import {DISHES} from './shared/dishes';
+import { Component } from 'react';
 
+class App extends Component{
+  constructor(props){
+    super(props);
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Giwang</NavbarBrand>
-        </div>
-      </Navbar>
-    </div>
-  );
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+  render(){
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Giwang</NavbarBrand>
+            
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
+
 }
-
 export default App;
