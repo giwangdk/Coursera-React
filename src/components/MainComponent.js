079a1e37563bdea6ca7a2b266c152b7d3be229b5
gidwikintan <gidwikintan@gmail.com>
2021-06-08 10:28:59 +0700

    controlled form

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,6 +8,7 @@ import {PROMOTIONS} from '../shared/promotions';
 import { Component } from 'react';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import About from './AboutComponent';
 import Contact from './ContactComponent';
 import {Switch, Route, Redirect} from 'react-router-dom';
 
@@ -49,6 +50,7 @@ class Main extends Component{
                 <Route path="/home" component={HomePage} />
                 <Route exact path="/menu" component={()=> <Menu dishes={this.state.dishes}/>} />
                 <Route path="/menu/:dishId" component={DishWithId} />
+                <Route path="/aboutus" component={()=> <About leaders={this.state.leaders}/>} />
                 <Route exact path="/contactus" component={Contact} />
                 <Redirect to="/home" />
             </Switch>
