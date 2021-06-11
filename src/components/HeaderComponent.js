ad2d1c414987b17e526a41518ee123e6cac963bf
gidwikintan <gidwikintan@gmail.com>
2021-06-11 13:50:21 +0700

    intro redux

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -27,7 +27,7 @@ class Header extends Component{
     }
 
     handleLogin(event){
-        this.toggleModal();
+        this.toggleModal();  
         alert("Username: "+this.username.value + "Password : " + this.password.value + "Remember: " +this.remember.checked);
         event.preventDefault();
     }
