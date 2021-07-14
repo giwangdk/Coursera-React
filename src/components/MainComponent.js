3889bf7b7294c3c2b0a60da89f3c959aa88ace7e
gidwikintan <gidwikintan@gmail.com>
2021-07-15 03:54:59 +0700

    add actioncreators, action types of leaders.

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,7 +8,7 @@ import About from './AboutComponent';
 import Contact from './ContactComponent';
 import {Switch, Route, Redirect, withRouter} from 'react-router-dom';
 import {connect} from 'react-redux';
-import { postComment, fetchDishes, fetchComments, fetchPromos} from '../redux/ActionCreators';
+import { postComment, fetchDishes, fetchComments, fetchPromos, fetchLeaders} from '../redux/ActionCreators';
 import {actions} from 'react-redux-form';
 import {TransitionGroup, CSSTransition} from 'react-transition-group';
 
@@ -27,6 +27,7 @@ const mapDispatchToProps = dispatch=>({
     resetFeedbackForm:()=>{dispatch(actions.reset('feedback'))},
     fetchComments:()=>{dispatch(fetchComments())},
     fetchPromos:()=>{dispatch(fetchPromos())},
+    fetchLeaders:()=>{dispatch(fetchLeaders())},
 })
 
 class Main extends Component{
@@ -37,6 +38,7 @@ class Main extends Component{
         this.props.fetchDishes();
         this.props.fetchComments();
         this.props.fetchPromos();
+        this.props.fetchLeaders();
     }
 
     render(){
@@ -48,7 +50,9 @@ class Main extends Component{
                     promotion={this.props.promotions.promotions.filter((promo)=>promo.featured)[0]}
                     promoLoading={this.props.promotions.isLoading}
                     promoErrMess={this.props.promotions.errMess}
-                    leader={this.props.leaders.filter((leader)=>leader.featured)[0]}
+                    leader={this.props.leaders.leaders.filter((leader)=>leader.featured)[0]}
+                    leadersLoading={this.props.leaders.isLoading}
+                    leadersErrMess={this.props.leaders.errMess}
                 />
             )
         }
